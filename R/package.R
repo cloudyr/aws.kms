0e9e017ee487af099ccb8fd95bf25e1b5bb8deeb
Thomas J. Leeper <thosjleeper@gmail.com>
2018-04-09 21:24:51 +0100

    add gh token for drat

--- R/package.R
+++ R/package.R
@@ -2,7 +2,7 @@
 #' @title aws.kms
 #' @aliases aws.kms-package aws.kms
 #' @docType package
-#' @description A Cloudyr Project Package
+#' @description AWS Key Management Service (KMS) Client.
 #' @details This is a client for the AWS Key Management Service (KMS).
 #' @author Thomas J. Leeper <thosjleeper@gmail.com>
 #' @seealso \code{\link{create_kms_key}}, \code{\link{list_kms_keys}}, \code{\link{generate_blob}}, \code{\link{encrypt}}
